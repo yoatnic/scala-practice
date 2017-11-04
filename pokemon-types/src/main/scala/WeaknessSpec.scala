3dc833d736179189923f846d6b65de990928e8af
yoat <yoatnic@me.com>
2017-11-04 17:32:43 +0900

    [4th itr]test be green

--- pokemon-types/src/main/scala/WeaknessSpec.scala
+++ pokemon-types/src/main/scala/WeaknessSpec.scala
@@ -2,6 +2,11 @@ class Weakness(val targetType: TypeEnum) {
   def types(): Set[TypeEnum] = {
     targetType match {
       case Fire => Set(Water, Rock)
+      case Water => Set(Glass)
+      case Glass => Set(Fire)
+      case Rock => Set(Water, Glass)
+      case Ghost => Set()
+      case Fight => Set()
     }
   }
 }
