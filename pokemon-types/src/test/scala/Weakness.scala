2c4868dba760bae36eb83ac58f63ade7c65a1324
yoat <yoatnic@me.com>
2017-11-04 17:32:10 +0900

    [4th itr]add all weakness test

--- pokemon-types/src/test/scala/Weakness.scala
+++ pokemon-types/src/test/scala/Weakness.scala
@@ -5,4 +5,29 @@ class WeaknessSpec extends FlatSpec with DiagrammedAssertions {
     var weakness: Weakness = new Weakness(Fire)
     assert(weakness.types() === Set(Water, Rock))
   }
+
+  it should "みず has weakness types くさ" in {
+    var weakness: Weakness = new Weakness(Water)
+    assert(weakness.types() === Set(Glass))
+  }
+
+  it should "くさ has weakness types ほのお" in {
+    var weakness: Weakness = new Weakness(Glass)
+    assert(weakness.types() === Set(Fire))
+  }
+
+  it should "いわ has weakness types みず, くさ" in {
+    var weakness: Weakness = new Weakness(Rock)
+    assert(weakness.types() === Set(Water, Glass))
+  }
+
+  it should "ゴースト has weakness types none" in {
+    var weakness: Weakness = new Weakness(Ghost)
+    assert(weakness.types() === Set())
+  }
+
+  it should "かくとう has weakness types none" in {
+    var weakness: Weakness = new Weakness(Fight)
+    assert(weakness.types() === Set())
+  }
 }
