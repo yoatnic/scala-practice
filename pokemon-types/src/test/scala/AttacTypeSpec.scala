516dda6dbb938cf8767d005eeda1fc2715ec366a
yoat <yoatnic@me.com>
2017-11-04 17:02:20 +0900

    [3rd itr]refactor, add test reduce dupplication type

--- pokemon-types/src/test/scala/AttacTypeSpec.scala
+++ pokemon-types/src/test/scala/AttacTypeSpec.scala
@@ -2,12 +2,12 @@ import org.scalatest._
 
 class AttacTypeSpec extends FlatSpec with DiagrammedAssertions {
   it should "type name is ほのお" in {
-    var attackType: AttackType = new AttackType("ほのお")
+    var attackType: AttackType = new AttackType(Fire)
     assert(attackType.name() === "ほのお")
   }
 
   it should "type name is みず" in {
-    var attackType: AttackType = new AttackType("みず")
+    var attackType: AttackType = new AttackType(Water)
     assert(attackType.name() === "みず")
   }
 }
--- pokemon-types/src/test/scala/DeffenceTypeSpec.scala
+++ pokemon-types/src/test/scala/DeffenceTypeSpec.scala
@@ -2,12 +2,12 @@ import org.scalatest._
 
 class DeffenceTypeSpec extends FlatSpec with DiagrammedAssertions {
   it should "type name is ほのお" in {
-    var deffenceType: DeffenceType = new DeffenceType("ほのお")
+    var deffenceType: DeffenceType = new DeffenceType(Fire)
     assert(deffenceType.name() === "ほのお")
   }
 
   it should "type name is みず" in {
-    var deffenceType: DeffenceType = new DeffenceType("みず")
+    var deffenceType: DeffenceType = new DeffenceType(Water)
     assert(deffenceType.name() === "みず")
   }
 }
