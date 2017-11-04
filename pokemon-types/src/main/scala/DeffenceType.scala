c107d7857c992e2199a2366dc67f171fd7ebccc7
yoat <yoatnic@me.com>
2017-11-04 17:39:14 +0900

    [4th itr]test be green

--- pokemon-types/src/main/scala/DeffenceType.scala
+++ pokemon-types/src/main/scala/DeffenceType.scala
@@ -2,4 +2,9 @@ class DeffenceType(val deffenceType: TypeEnum) {
   def name(): String = {
     deffenceType.name
   }
+
+  def weakness(): Set[TypeEnum] = {
+    var w: Weakness = new Weakness(deffenceType)
+    w.types()
+  }
 }
