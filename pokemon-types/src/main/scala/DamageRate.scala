c83eb1e64b56ac14ee771a18c24f091aa6be2d23
yoat <yoatnic@me.com>
2017-11-04 19:18:32 +0900

    [9th itr]test be green

--- pokemon-types/src/main/scala/DamageRate.scala
+++ pokemon-types/src/main/scala/DamageRate.scala
@@ -3,6 +3,9 @@ class DamageRate() {
     if (deffence.weakness()(attack.typeof())) {
       return 2
     }
+    if (deffence.reduceness()(attack.typeof())) {
+      return 0.5
+    }
     return 1
   }
 }
