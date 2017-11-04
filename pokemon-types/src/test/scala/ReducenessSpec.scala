080e710f9e3b1f678565f01194bb7e53770c6723
yoat <yoatnic@me.com>
2017-11-04 18:06:03 +0900

    [5th itr] add all reduceness test

--- pokemon-types/src/test/scala/ReducenessSpec.scala
+++ pokemon-types/src/test/scala/ReducenessSpec.scala
@@ -5,4 +5,29 @@ class ReducenessSpec extends FlatSpec with DiagrammedAssertions {
     var reduceness: Reduceness = new Reduceness(Fire)
     assert(reduceness.types() === Set(Glass))
   }
+
+  it should "みず has reduceness types ほのお" in {
+    var reduceness: Reduceness = new Reduceness(Water)
+    assert(reduceness.types() === Set(Fire))
+  }
+
+  it should "くさ has reduceness types みず" in {
+    var reduceness: Reduceness = new Reduceness(Glass)
+    assert(reduceness.types() === Set(Water))
+  }
+
+  it should "いわ has reduceness types ほのお" in {
+    var reduceness: Reduceness = new Reduceness(Rock)
+    assert(reduceness.types() === Set(Fire))
+  }
+
+  it should "ゴースト has reduceness types none" in {
+    var reduceness: Reduceness = new Reduceness(Ghost)
+    assert(reduceness.types() === Set())
+  }
+
+  it should "かくとう has reduceness types いわ" in {
+    var reduceness: Reduceness = new Reduceness(Fight)
+    assert(reduceness.types() === Set(Rock))
+  }
 }
