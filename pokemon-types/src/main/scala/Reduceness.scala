de93a69c5ffecbce61dda80d66fddfe46d5b8da6
yoat <yoatnic@me.com>
2017-11-04 18:06:22 +0900

    [5th itr] test be green

--- pokemon-types/src/main/scala/Reduceness.scala
+++ pokemon-types/src/main/scala/Reduceness.scala
@@ -2,6 +2,11 @@ class Reduceness(val targetType: TypeEnum) {
   def types(): Set[TypeEnum] = {
     targetType match {
       case Fire => Set(Glass)
+      case Water => Set(Fire)
+      case Glass => Set(Water)
+      case Rock => Set(Fire)
+      case Ghost => Set()
+      case Fight => Set(Rock)
     }
   }
 }
